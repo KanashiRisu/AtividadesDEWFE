6f1e264b55b994e4fe0102425a52a2d0b365cb9a
Murilo Paludetto <122514775+KanashiRisu@users.noreply.github.com>
2026-05-20 21:57:10 -0300

    scripts e continuação de bootstrap

---
+++ Atividade15/scripts/script.js
@@ -0,0 +1,18 @@
+//  function corVermelha() {
+//      document.body.style.backgroundColor = 'red';
+//  }
+
+//  function corVerde() {
+//     document.body.style.backgroundColor = 'green';
+//  }
+
+//  function corAzul() {
+//      document.body.style.backgroundColor = 'blue';
+//  }
+
+
+//Altera cor com base no argumento recebido -> 'cor'
+function mudaCor(cor) {
+    document.body.style.backgroundColor = cor;
+}
+
